4de92dc48043ab3c20cd1a2fa9f347e4dd75fac6
Pannenets.F <you@example.com>
2021-03-13 16:37:30 +0800

    feat: basic torch.nn

---
+++ wageubn/function/__init__.py
@@ -0,0 +1,14 @@
+from torch.nn import Module, ModuleList, ModuleDict, Sequential
+
+from torch.nn import Parameter
+
+from torch.nn import MaxPool1d, MaxPool2d, MaxPool3d
+from torch.nn import AvgPool1d, AvgPool2d, AvgPool3d
+
+from .activation import ReLU, ReLU6
+from .batchnorm import BatchNorm2d
+from .conv import Conv2d
+from .linear import Linear
+
+# from . import extra
+# from .adder import Adder2d
