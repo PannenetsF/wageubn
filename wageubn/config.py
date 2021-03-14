b8b408801f60bdd21f7931cdb6382e97783f719b
Pannenets.F <you@example.com>
2021-03-14 16:35:50 +0800

    feat: make config more flexible

--- wageubn/config.py
+++ wageubn/config.py
@@ -8,12 +8,8 @@ Config = namedtuple('config', [
 ],
                     defaults=[8, 8, 8, 8, 8, 8, 8, 8])
 
-bn_list = [nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d]
-acti_list = [nn.ReLU, nn.ReLU6]
-conv_linear_list = [nn.Conv1d, nn.Conv2d, nn.Conv3d, nn.Linear]
 
-
-def config_bn(proc, config):
+def config_bn(proc, config, bn_list):
     if _isinstance(proc, bn_list):
         proc.weight_bit_width = config.bn_weight
         proc.bias_bit_width = config.bn_bias
@@ -22,25 +18,38 @@ def config_bn(proc, config):
         proc.bn_bit_width = config.bn_out
 
 
-def config_acti(proc, config):
+def config_acti(proc, config, acti_list):
     if _isinstance(proc, acti_list):
         proc.acti_bit_width = config.acti
 
 
-def config_conv_linear(proc, config):
+def config_conv_linear(proc, config, conv_linear_list):
     if _isinstance(proc, conv_linear_list):
         proc.weight_bit_width = config.conv_and_linear_weight
         proc.bias_bit_width = config.conv_and_linear_bias
 
 
-def config_network(net, name, config, show=False):
+def config_network(
+        net,
+        name,
+        config,
+        bn_list=[nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d],
+        acti_list=[nn.ReLU, nn.ReLU6],
+        conv_linear_list=[nn.Conv1d, nn.Conv2d, nn.Conv3d, nn.Linear],
+        show=False):
     proc_list = list(net._modules.keys())
     if proc_list == []:
         if show:
             print(name, 'is configured')
-        config_acti(net, config)
-        config_conv_linear(net, config)
-        config_bn(net, config)
+        config_acti(net, config, acti_list=acti_list)
+        config_conv_linear(net, config, conv_linear_list=conv_linear_list)
+        config_bn(net, config, bn_list=bn_list)
     else:
         for n in proc_list:
-            config_network(net._modules[n], name + '.' + n, config, show=show)
+            config_network(net._modules[n],
+                           name + '.' + n,
+                           config,
+                           bn_list=bn_list,
+                           acti_list=acti_list,
+                           conv_linear_list=conv_linear_list,
+                           show=show)
