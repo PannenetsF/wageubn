25aac7e07da9b80b51620a8862c61504c0ab9248
Pannenets.F <you@example.com>
2021-03-14 11:43:51 +0800

    feat: add addernet support

--- wageubn/function/__init__.py
+++ wageubn/function/__init__.py
@@ -10,5 +10,5 @@ from .batchnorm import BatchNorm2d
 from .conv import Conv2d
 from .linear import Linear
 
-# from . import extra
-# from .adder import Adder2d
+from . import extra
+from .adder import Adder2d
