1e54409971ab5cd62f119e5e9e3b533aa96243cf
pan <pan>
2021-03-15 23:29:50 +0800

    feat: iostrict conv

--- wageubn/function/conv.py
+++ wageubn/function/conv.py
@@ -10,20 +10,21 @@ from .number import directquant
 
 
 class Conv2d(nn.Conv2d):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        kernel_size,
-        stride=1,
-        padding=0,
-        dilation=1,
-        groups=1,
-        bias=True,
-        padding_mode='zeros',
-        weight_bit_width=8,
-        bias_bit_width=16,
-    ):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 padding=0,
+                 dilation=1,
+                 groups=1,
+                 bias=True,
+                 padding_mode='zeros',
+                 weight_bit_width=8,
+                 bias_bit_width=16,
+                 input_bit_width=8,
+                 output_bit_width=12,
+                 iostrict=False):
         super().__init__(in_channels=in_channels,
                          out_channels=out_channels,
                          kernel_size=kernel_size,
@@ -35,15 +36,23 @@ class Conv2d(nn.Conv2d):
                          padding_mode=padding_mode)
         self.weight_bit_width = weight_bit_width
         self.bias_bit_width = bias_bit_width
+        self.input_bit_width = input_bit_width
+        self.output_bit_width = self.output_bit_width
+        self.iostrict = iostrict
 
     def conv_forward(self, input):
+        if self.iostrict is True:
+            input = directquant(input, self.input_bit_width)
         if self.bias is None:
             bias = None
         else:
             bias = directquant(self.bias, self.bias_bit_width)
         weight = directquant(self.weight, self.weight_bit_width)
-        return F.conv2d(input, weight, bias, self.stride, self.padding,
+        output F.conv2d(input, weight, bias, self.stride, self.padding,
                         self.dilation, self.groups)
+        if self.iostrict is True:
+            output = directquant(output, self.output_bit_width)
+        return output
 
     def forward(self, input):
         return self.conv_forward(input)
