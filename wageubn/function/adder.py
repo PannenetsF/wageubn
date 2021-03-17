5c214f489fde6d83705f5a1101174168acc077ea
pan <pan>
2021-03-17 22:46:02 +0800

    feat: all quant adder

--- wageubn/function/adder.py
+++ wageubn/function/adder.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 from torch.autograd import Function
 
 from . import extra as ex
-from .number import directquant
+from .number import directquant, alldirectquant
 
 
 class Adder2d(ex.Adder2d):
@@ -23,6 +23,10 @@ class Adder2d(ex.Adder2d):
                  bias_dec_bit_width=16,
                  input_dec_bit_width=8,
                  output_dec_bit_width=12,
+                 weight_all_bit_width=8,
+                 bias_all_bit_width=16,
+                 input_all_bit_width=8,
+                 output_all_bit_width=12,
                  iostrict=False):
         super().__init__(input_channel,
                          output_channel,
@@ -30,6 +34,10 @@ class Adder2d(ex.Adder2d):
                          stride=stride,
                          padding=padding,
                          bias=bias)
+        self.weight_all_bit_width = weight_all_bit_width
+        self.bias_all_bit_width = bias_all_bit_width
+        self.input_all_bit_width = input_all_bit_width
+        self.output_all_bit_width = output_all_bit_width
         self.weight_dec_bit_width = weight_dec_bit_width
         self.bias_dec_bit_width = bias_dec_bit_width
         self.input_dec_bit_width = input_dec_bit_width
@@ -38,19 +46,23 @@ class Adder2d(ex.Adder2d):
 
     def adder_forward(self, input):
         if self.iostrict is True:
-            input = directquant(input, self.input_dec_bit_width)
+            input = alldirectquant(input, self.input_dec_bit_width,
+                                   self.input_all_bit_width)
         if self.bias is None:
             bias = None
         else:
-            bias = directquant(self.bias, self.bias_dec_bit_width)
-        weight = directquant(self.weight, self.weight_dec_bit_width)
+            bias = alldirectquant(self.bias, self.bias_dec_bit_width,
+                                  self.bias_all_bit_width)
+        weight = alldirectquant(self.weight, self.weight_dec_bit_width,
+                                self.weight_all_bit_width)
         output = ex.adder2d_function(input,
                                      weight,
                                      bias,
                                      stride=self.stride,
                                      padding=self.padding)
         if self.iostrict is True:
-            output = directquant(output, self.output_dec_bit_width)
+            output = alldirectquant(output, self.output_dec_bit_width,
+                                    self.output_all_bit_width)
         return output
 
     def forward(self, input):
