860572b784c9b70b65762cd2154a7976020cba27
pan <pan>
2021-03-17 22:38:08 +0800

    feat: all quant acti

--- wageubn/function/activation.py
+++ wageubn/function/activation.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .number import directquant
+from .number import directquant, alldirectquant
 
 
 class ReLU(nn.ReLU):
@@ -14,36 +14,44 @@ class ReLU(nn.ReLU):
                  inplace=False,
                  input_dec_bit_width=8,
                  acti_dec_bit_width=8,
+                 input_all_bit_width=8,
+                 acti_all_bit_width=8,
                  iostrict=False):
         super().__init__(inplace)
         self.acti_dec_bit_width = acti_dec_bit_width
         self.input_dec_bit_width = input_dec_bit_width
+        self.acti_all_bit_width = acti_all_bit_width 
+        self.input_all_bit_width input_all_bit_width 
         self.iostrict = iostrict
 
     def relu_forward(self, input):
         if self.iostrict is True:
-            input = directquant(input, self.input_dec_bit_width)
-        return directquant(F.relu(input), self.acti_dec_bit_width)
+            input = alldirectquant(input, self.input_dec_bit_width, self.input_all_bit_width)
+        return alldirectquant(F.relu(input), self.acti_dec_bit_width, self.acti_all_bit_width)
 
     def forward(self, input):
         return self.relu_forward(input)
 
 
-class ReLU6(nn.ReLU6):
+class ReLU6(nn.ReLU):
     def __init__(self,
                  inplace=False,
-                 acti_dec_bit_width=8,
                  input_dec_bit_width=8,
+                 acti_dec_bit_width=8,
+                 input_all_bit_width=8,
+                 acti_all_bit_width=8,
                  iostrict=False):
         super().__init__(inplace)
         self.acti_dec_bit_width = acti_dec_bit_width
         self.input_dec_bit_width = input_dec_bit_width
+        self.acti_all_bit_width = acti_all_bit_width 
+        self.input_all_bit_width input_all_bit_width 
         self.iostrict = iostrict
 
     def relu6_forward(self, input):
         if self.iostrict is True:
-            input = directquant(input, self.input_dec_bit_width)
-        return directquant(F.relu6(input), self.acti_dec_bit_width)
+            input = alldirectquant(input, self.input_dec_bit_width, self.input_all_bit_width)
+        return alldirectquant(F.relu6(input), self.acti_dec_bit_width, self.acti_all_bit_width)
 
     def forward(self, input):
         return self.relu6_forward(input)
