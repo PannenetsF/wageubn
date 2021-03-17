3ae0f3fbb41527d710aa92ee83a93e819b423dd9
pan <pan>
2021-03-17 22:41:15 +0800

    feat: all quant bn

--- wageubn/function/batchnorm.py
+++ wageubn/function/batchnorm.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .number import directquant
+from .number import directquant, alldirectquant
 
 
 class BatchNorm2d(nn.BatchNorm2d):
@@ -22,6 +22,12 @@ class BatchNorm2d(nn.BatchNorm2d):
                  var_dec_bit_width=8,
                  output_dec_bit_width=8,
                  input_dec_bit_width=8,
+                 weight_all_bit_width=8,
+                 bias_all_bit_width=8,
+                 mean_all_bit_width=8,
+                 var_all_bit_width=8,
+                 output_all_bit_width=8,
+                 input_all_bit_width=8,
                  iostrict=False):
         super().__init__(num_features, eps, momentum, affine,
                          track_running_stats)
@@ -32,26 +38,38 @@ class BatchNorm2d(nn.BatchNorm2d):
         self.var_dec_bit_width = var_dec_bit_width
         self.output_dec_bit_width = output_dec_bit_width
         self.input_dec_bit_width = input_dec_bit_width
+        self.weight_all_bit_width = weight_all_bit_width
+        self.bias_all_bit_width = bias_all_bit_width
+        self.mean_all_bit_width = mean_all_bit_width
+        self.var_all_bit_width = var_all_bit_width
+        self.output_all_bit_width = output_all_bit_width
+        self.input_all_bit_width = input_all_bit_width
         self.iostrict = iostrict
 
     def bn_forward(self, input):
         if self.iostrict is True:
-            input = directquant(input, self.input_dec_bit_width)
-        mean = directquant(self.running_mean, self.mean_dec_bit_width)
-        var = directquant(self.running_var, self.var_dec_bit_width)
+            input = alldirectquant(input, self.input_dec_bit_width,
+                                   self.input_all_bit_width)
+        mean = alldirectquant(self.running_mean, self.mean_dec_bit_width,
+                              self.mean_all_bit_width)
+        var = alldirectquant(self.running_var, self.var_dec_bit_width,
+                             self.var_all_bit_width)
         if self.affine is True:
-            weight = directquant(self.weight, self.weight_dec_bit_width)
-            bias = directquant(self.bias, self.bias_dec_bit_width)
-            output = directquant(
+            weight = alldirectquant(self.weight, self.weight_dec_bit_width,
+                                    self.weight_all_bit_width)
+            bias = alldirectquant(self.bias, self.bias_dec_bit_width,
+                                  self.bias_all_bit_width)
+            output = alldirectquant(
                 F.batch_norm(input,
                              running_mean=mean,
                              running_var=var,
                              weight=weight,
-                             bias=bias), self.output_dec_bit_width)
+                             bias=bias), self.output_dec_bit_width,
+                self.output_all_bit_width)
         else:
-            output = directquant(
+            output = alldirectquant(
                 F.batch_norm(input, running_mean=mean, running_var=var),
-                self.output_dec_bit_width)
+                self.output_dec_bit_width, self.output_all_bit_width)
         return output
 
     def forward(self, input):
